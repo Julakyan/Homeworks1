80c2cf8b6a7600b8c843edb2126231b22e3a3f9b
Julakyan <131914675+Julakyan@users.noreply.github.com>
2023-05-25 17:14:43 +0400

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,55 +1,100 @@
-//1
-let i =0
-function countDigits(n){
-    for(i = 0; n > 1 ;i++){
-        n/=10;
-    }
 
-    return i;
+function test(string,a,b){
+    string = string.split("")
+    for(let i = 0; i<string.length;i++){
+        if(string[i] === a){
+            string[i] = b
+        }
+    }
+    return string.join("")
+    
+}
+
+console.log(test("Armen","A","B")); 
+
+// ------------------------------------------------------------------------
+
+let getCountSymbols = (text, a) =>{
+    let count = 0
+    let i = 0
+    while(i<text.length){
+        if(text[i]===a){
+            count++
+            } 
+        i++
+
+     } 
+     return count
 } 
 
-countDigits()
+console.log(getCountSymbols('hhhadsdfjjjj','j'))
+
+ // ----------------------------------------------------------
+
+ let num = 10
 
 
+for(let i = 1; i<=num;i++){
+    let number = ""
+    for(let j = 1; j <= i;j++){
+        number = number + j
+    }
+    console.log(number)
+}
 
+for(let i = num-1; i>=1;i--){
+    let number = ""
+    for(let j = 1; j <= i;j++){
+        number = number + j
+    }
+    console.log(number)
+}
 
-//2
-function checkForZeros(number) {
-    let count = 0;
-    let temp = number;
+// ----------------------------------------------------------------------------
 
-    while (temp > 0) {
-        const digit = temp % 10;
-        temp = Math.floor(temp / 10);
+function checkPalindrome(string) {
 
-        if (digit === 0) {
-            count++;
+    const len = string.length;
 
-            if (count === 3) {
-                return 'yes';
-            }
-        } else {
-            count = 0;
+    for (let i = 0; i < len / 2; i++) {
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
         }
     }
 
-    return 'no';
+    return 'It is a palindrome';
 }
-checkForZeros()
-//3
-function checkForNines(number) {
-        let count = 0;
-        let temp = number;
-    
-        while (temp > 0) {
-            const digit = temp % 10;
-            temp = Math.floor(temp / 10);
-    
-            if (digit === 9) {
-                count++;
-        }
-    }
-    return count;
-    }
-    checkForNines()
 
+
+
+console.log(checkPalindrome("boooooooooobb"));
+
+// ---------------------------------------------------
+let num = 5
+let result = ""
+for(let i = 0; i<num;i++){
+    for(let j = 0; j < num;j++){
+        if(i === j || j === num - 1 - i){
+            result+=" * "
+        } else {
+            result+= "   "
+        }
+    }
+    result+= "\n"
+}
+console.log(result)
+
+//-------------------------------------------
+let num = 5
+let result = ""
+for(let i = 0; i<num;i++){
+    for(let j = 0; j < num;j++){
+        if(i === j){
+            result+=" * "
+        } else {
+            result+= "   "
+        }
+    }
+    result+= "\n"
+}
+console.log(result)

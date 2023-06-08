b8976086885d5a06d463c142c4d0deb2df0ed9ae
Julakyan <131914675+Julakyan@users.noreply.github.com>
2023-06-08 07:46:19 +0400

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,81 +1,79 @@
 //1
+let obj = {
+    a:"1",
+    b:"2",
+    c:"2",
+    d:"2",
+    e:"3",
+    t:"3"
+}
 
-function checkPrimeNumber(number){
-    if(number === 1 || number === 2){
-        return true
-    } else {
-        for(let i = 2; i < number; i++){
-            if(number % i === 0){
-                return false
-            }
+function invert(object){
+    let res = {}
+    for(key in object){
+        if(res.hasOwnProperty(object[key])){
+            res[object[key]] = res[object[key]] + key    
+        } else {
+            res[object[key]] = key
         }
-        return true
-    }
-
-}
-
-console.log(checkPrimeNumber())
-
-//-------------------------------------------------------------------------
-
-// 2  --- the first function is used here ~ RECOMMENT for work
-
-function primeNumbersBeforeGivenNumber(number){
-    const n = 100;
-for (let i = 1; i <= n; i++) {
-  for (let j = 2; j <= i; j++) {
-    if (i % j === 0 && j < i) {
-      break;
-    } else if (j === i) {
-      console.log(i);
-    }
-  }
-}
         
     }
+    console.log(res);
+}
 
-    primeNumbersBeforeGivenNumber()
+invert(obj)
 
+//----------------------------
 
-    //---------------------------------------------------------------------------
+//2
+
+var a = {a:'1',b:"1"}
+var b = {a:'1',b:"1"}
+
+function shallowCompare(a,b){
+    let result
+    for(key in a){
+        if(b.hasOwnProperty(key)){
+            if(a[key] === b[key]){
+                continue;
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+    }
+    for(key in b){
+        if(a.hasOwnProperty(key)){
+            if(b[key] === a[key]){
+                continue;
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+    }
+    return true
+    
+}
+console.log(shallowCompare(a,b));
 
 //3
 
+let text = "testText"
 
-function sumNumbersOfString(text){
-        let temp = "0";
-        let sum = 0;
-   
-        for (let i = 0; i < text.length; i++) {
-            let ch = text[i];
-   
-            if (!isNaN(String(ch) * 1)){
-                temp += ch;
-            }
-                
-            else {
-                sum += +temp;
-                temp = "0";
-            }
-        }
-        return sum + parseInt(temp);
+function textIsIsogram(text){
+    let words = {}
+    for(let i = 0; i<text.length; i++){
+        if(!words.hasOwnProperty(text[i])){
+            words[text[i]] = text[i]
+        } else {
+            return false
+        }        
+    } 
+    return true
 }
 
-console.log(sumNumbersOfString("fjd25dfds25jsdhf5"))
+console.log(textIsIsogram(text))
 
-
-4 
-
-    function removeWordOfString(text,word){
-        let myText = text.split(" ");
-        console.log(myText)
-        for(let i = 0; i < myText.length; i++){
-            if(myText[i] === word){
-                myText = myText.slice(0,i).join(" ") + " " + myText.slice(i + 1 , myText.length).join(" ")
-            }
-        }
-        console.log(myText)
-        return myText
-    }
-
-    console.log(removeWordOfString("this is some text","is"));
